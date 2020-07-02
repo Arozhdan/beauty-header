5f16153f7fecae8b4de4ef9e2a0f10c4084fadf4
arozhdan <halala30@gmail.com>
2020-07-02 23:00:19 +0200

    done

---
+++ app/js/main.js
@@ -0,0 +1,63 @@
+$(function () {
+    var rellax = new Rellax('.rellax', {
+        speed: -2,
+        center: false,
+        wrapper: null,
+        round: true,
+        vertical: true,
+        horizontal: false
+    });
+    $('.header-top-address-link').on('mouseover', () => {
+        toggleMap()
+    })
+    $('.header-top-address-img').on('mouseover', () => {
+        toggleMap()
+    })
+    $('#toggle-info').on('mouseover', () => {
+        $('.header-content-inner-right-panel').show()
+        $('.header-content-inner-right-panel').on('mouseleave', () => {
+            $('.header-content-inner-right-panel').hide()
+        })
+    })
+    $('.header-top-contacts-btn').on('click', () => {
+        showForm()
+
+    })
+    $('.header-content-btn').on('click', () => {
+        showForm()
+    })
+    $('.form-submit').on('click', (e) => {
+        e.preventDefault()
+        $('.popup').hide()
+        $('.form-thanks-wrapper').show()
+        $('.form-thanks-close').on('click', () => {
+            $('.form-thanks-wrapper').hide()
+        })
+    })
+});
+
+function toggleMap() {
+    $('.map').show()
+    $('.map').on('mouseleave', () => {
+        $('.map').hide()
+    })
+    $(document).on('click', (e) => {
+        if (!$(e.target).hasClass('map') && !$(e.target).hasClass('header-top-address-link')) {
+            $('.map').hide()
+        }
+    })
+
+}
+
+function showForm() {
+    $('.popup').show()
+    $('.popup-form-close').on('click', e => {
+        e.preventDefault()
+        $('.popup').hide()
+    })
+    $(document).keydown(function (e) {
+        if (e.keyCode === 27) {
+            $('.popup').hide()
+        }
+    });
+}
